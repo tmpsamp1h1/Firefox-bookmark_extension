56f33af4c47a29340d7c2e8b66eb06d8cf76c703
tmpsam22 <tmpsaminf@gmail.com>
2022-09-10 17:12:44 +0700

    refactored popup & added shortcut

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,9 @@
 const storage = browser.storage.local;
 const tabs = browser.tabs;
-
+const commands = browser.commands;
 const path_to_storage_page = '../storage/storage.html';
 
-// @todo: enter -> add or edit !
-// @todo: new popup window that user edit or saved to more information 
+// @todo: new popup window that user edit or saved to more information  ???? 
 // @todo: set changeble icon( redraw after adding )
 
 function reload_values( switchToEdit = false )
@@ -25,6 +24,57 @@ function reload_values( switchToEdit = false )
      status.innerHTML = 'Add bookmark';
      cancel.innerHTML = 'Cancel';
      add.innerHTML = 'Add';
+     document.getElementById('description').focus();
+}
+
+function insert_to_storage(currentUrl)
+{
+     const bookmark_to_insert = {
+          url: currentUrl,
+          title: document.getElementById('title').value,
+          description: document.getElementById('description').value,
+          date : { 
+               added : new Date().toLocaleString(),
+               changed : ''
+          }
+     };
+
+     storage.get().then(data =>
+     {
+          if (!data.bookmarks)
+          {
+               storage.set(
+               {
+                    bookmarks: [bookmark_to_insert]
+               });
+               let switchValuesToEdit = true;
+               reload_values( switchValuesToEdit );
+               return;
+          }
+
+          const indexBookmark = data.bookmarks.findIndex(
+               item => item['url'] === currentUrl
+          );
+
+          // edit bookmark which is already in the storage
+          if (indexBookmark != -1)
+          {
+               data.bookmarks[indexBookmark]['title'] = document.getElementById('title').value;
+               data.bookmarks[indexBookmark]['description'] = document.getElementById('description').value;
+               data.bookmarks[indexBookmark]['date']['changed'] = new Date().toLocaleString();
+          }
+          else // it's a new bookmark
+          {
+               data.bookmarks.push(bookmark_to_insert);
+               let switchValuesToEdit = true;
+               reload_values( switchValuesToEdit );
+          }
+          storage.set(
+          {
+               bookmarks: data.bookmarks
+          });
+          
+     });
 }
 
 function setup_click_action( currentUrl )
@@ -33,70 +83,48 @@ function setup_click_action( currentUrl )
 
      document.getElementById('storage').addEventListener('click', () =>
      {
-          window.close();
-          window.open( path_to_storage_page );
+          tabs.create( {
+               url: path_to_storage_page
+          } ).then( () => { 
+               window.close()
+          } );
      });
 
      document.getElementById('cancel').addEventListener('click', () =>
      {
-          if ( cancel = document.getElementById('cancel').innerHTML == 'Cancel' )
+          if ( document.getElementById('cancel').innerHTML == 'Cancel' )
           {
                window.close();
-          } else {
-               reload_values();
+          } 
+          else
+          {
+               storage.get().then( data => {
+                    reload_values();
+                    const indexBookmark = data.bookmarks.findIndex(
+                         item => item['url'] === currentUrl
+                    );
+
+                    data.bookmarks.splice(indexBookmark, 1);
+                    storage.set(
+                    {
+                         bookmarks: data.bookmarks
+                    });
+                    document.getElementById('description').value = "";
+               } );
           }
      });
 
-     document.getElementById('add').addEventListener('click', () =>
-     {
-          const bookmark_to_insert = {
-               url: currentUrl,
-               title: document.getElementById('title').value,
-               description: document.getElementById('description').value,
-               date : { 
-                    added : new Date().toLocaleString(),
-                    changed : ''
-               }
-          };
-
-          storage.get().then(data =>
-          {
-               if (!data.bookmarks)
-               {
-                    storage.set(
-                    {
-                         bookmarks: [bookmark_to_insert]
-                    });
-                    return;
-               }
-
-               const indexBookmark = data.bookmarks.findIndex(
-                    item => item['url'] === currentUrl
-               );
-
-               // edit bookmark which is already in the storage
-               if (indexBookmark != -1)
-               {
-                    data.bookmarks[indexBookmark]['title'] = document.getElementById('title').value;
-                    data.bookmarks[indexBookmark]['description'] = document.getElementById('description').value;
-                    data.bookmarks[indexBookmark]['date']['changed'] = new Date().toLocaleString();
-               }
-               else // it's a new bookmark
-               {
-                    data.bookmarks.push(bookmark_to_insert);
-                    let switchValuesToEdit = true;
-                    reload_values( switchValuesToEdit );
-               }
-               storage.set(
-               {
-                    bookmarks: data.bookmarks
-               });
-
-          });
+     document.getElementById('add').addEventListener('click', () => {
+          insert_to_storage( currentUrl );
      });
+
+     commands.onCommand.addListener( command => {
+          if ( command == 'enter_action') {
+               insert_to_storage( currentUrl );
+          }
+     } );
 }
 
-// setup popup 
 tabs.query(
      {
           currentWindow: true,
@@ -110,7 +138,6 @@ tabs.query(
                
           setup_click_action( tabs[0].url );
 
-          document.getElementById('title').defaultValue = tabs[0].title;
           storage.get().then(data =>
           {
                if ( !data.bookmarks )
@@ -126,22 +153,17 @@ tabs.query(
                let buttonReset = document.getElementById('cancel');
                if (indexBookmark == -1)
                {
+                    document.getElementById('title').defaultValue = tabs[0].title;
                     buttonSubmit.firstChild.data = 'Add';
                     buttonReset.firstChild.data = 'Cancel'
                }
                else
                {
+                    document.getElementById('title').value = data.bookmarks[indexBookmark].title;
+                    document.getElementById('description').value = data.bookmarks[indexBookmark].description;
                     status.innerHTML = 'Edit bookmark';
                     buttonSubmit.firstChild.data = 'Edit';
                     buttonReset.firstChild.data = 'Remove';
-                    buttonReset.addEventListener('click', () =>
-                    {
-                         data.bookmarks.splice(indexBookmark, 1);
-                         storage.set(
-                         {
-                              bookmarks: data.bookmarks
-                         });
-                    });
                }
           })
      }
