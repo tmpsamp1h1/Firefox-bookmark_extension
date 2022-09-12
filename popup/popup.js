b1256f9704d4ab14e6d4cdd6be396d4ed4abc89b
tmpsam22 <tmpsaminf@gmail.com>
2022-09-13 00:16:56 +0700

    format+comments

--- popup/popup.js
+++ popup/popup.js
@@ -2,17 +2,21 @@ const storage = browser.storage.local;
 const tabs = browser.tabs;
 const commands = browser.commands;
 const browserAction = browser.browserAction;
+
 const path_to_storage_page = '../storage/storage.html';
 const path_to_icons = "../icons/";
 
-function reload_values( switchToEdit = false )
+/// @brief helper funtion that which switches button names
+/// @param switchToEdit - if it's true, then the bookmark is in the storage
+     /// should change behavior of buttons
+function rename_button_names(switchToEdit = false)
 {
      let status = document.getElementById('status');
      let buttons = document.getElementById('popup_buttons');
      let cancel = document.getElementById('cancel');
      let add = document.getElementById('add');
      
-     if ( switchToEdit )
+     if (switchToEdit)
      {
           status.innerHTML = 'Edit bookmark';
           cancel.innerHTML = 'Remove';
@@ -23,146 +27,182 @@ function reload_values( switchToEdit = false )
      status.innerHTML = 'Add bookmark';
      cancel.innerHTML = 'Cancel';
      add.innerHTML = 'Add';
-     document.getElementById('description').focus();
+     document.getElementById('description')
+          .focus();
 }
 
+/// @brief helper function that adds a bookmark to the storage
+/// @param currentUrl - the url to be added to the storage
 function insert_to_storage(currentUrl)
 {
      const bookmark_to_insert = {
-          url: currentUrl,
-          title: document.getElementById('title').value,
-          description: document.getElementById('description').value,
-          date : { 
-               added : new Date().toLocaleString(),
-               changed : ''
+          url: currentUrl
+          , title: document.getElementById('title')
+               .value
+          , description: document.getElementById('description')
+               .value
+          , date:
+          {
+               added: new Date()
+                    .toLocaleString()
+               , changed: ''
           }
      };
-
-     storage.get().then(data =>
-     {
-          if (!data.bookmarks)
+     
+     storage.get()
+          .then(data =>
           {
-               storage.set(
+               if (!data.bookmarks)
                {
-                    bookmarks: [bookmark_to_insert]
-               });
-               let switchValuesToEdit = true;
-               reload_values( switchValuesToEdit );
-               return;
-          }
-
-          const indexBookmark = data.bookmarks.findIndex(
-               item => item['url'] === currentUrl
-          );
-
-          // edit bookmark which is already in the storage
-          if (indexBookmark != -1)
-          {
-               data.bookmarks[indexBookmark]['title'] = document.getElementById('title').value;
-               data.bookmarks[indexBookmark]['description'] = document.getElementById('description').value;
-               data.bookmarks[indexBookmark]['date']['changed'] = new Date().toLocaleString();
-          }
-          else // it's a new bookmark
-          {
-               data.bookmarks.push(bookmark_to_insert);
-               let switchValuesToEdit = true;
-               reload_values( switchValuesToEdit );
-          }
-          storage.set(
-          {
-               bookmarks: data.bookmarks
-          });
-          
-     });
-}
-
-function setup_click_action( currentUrl )
-{
-     document.getElementById('description').focus();
-
-     document.getElementById('storage').addEventListener('click', () =>
-     {
-          tabs.create( {
-               url: path_to_storage_page
-          } ).then( () => { 
-               window.close()
-          } );
-     });
-
-     document.getElementById('cancel').addEventListener('click', () =>
-     {
-          if ( document.getElementById('cancel').innerHTML == 'Cancel' )
-          {
-               window.close();
-          } 
-          else
-          {
-               storage.get().then( data => {
-                    reload_values();
-                    const indexBookmark = data.bookmarks.findIndex(
-                         item => item['url'] === currentUrl
-                    );
-
-                    data.bookmarks.splice(indexBookmark, 1);
                     storage.set(
                     {
-                         bookmarks: data.bookmarks
+                         bookmarks: [bookmark_to_insert]
                     });
-                    document.getElementById('description').value = "";
-               } );
+                    let switchValuesToEdit = true;
+                    rename_button_names(switchValuesToEdit);
+                    return;
+               }
+               
+               const indexBookmark = data.bookmarks.findIndex(
+                    item => item['url'] === currentUrl
+               );
+               
+               // user edited a bookmark which is already in the storage
+               if (indexBookmark != -1)
+               {
+                    data.bookmarks[indexBookmark]['title'] = document.getElementById('title')
+                         .value;
+                    data.bookmarks[indexBookmark]['description'] = document.getElementById('description')
+                         .value;
+                    data.bookmarks[indexBookmark]['date']['changed'] = new Date()
+                         .toLocaleString();
+               }
+               else // user has just added a new bookmark
+               {
+                    data.bookmarks.push(bookmark_to_insert);
+                    let switchValuesToEdit = true;
+                    rename_button_names(switchValuesToEdit);
+               }
+
+               storage.set(
+               {
+                    bookmarks: data.bookmarks
+               });
+               
+          });
+}
+
+/// @brief helper function that connects button clicks with events
+/// @param currentUrl - the url to be added to the storage
+function setup_click_action(currentUrl)
+{
+     document.getElementById('description')
+          .focus();
+     
+     document.getElementById('storage')
+          .addEventListener('click', () =>
+          {
+               tabs.create(
+                    {
+                         url: path_to_storage_page
+                    })
+                    .then(() =>
+                    {
+                         window.close()
+                    });
+          });
+     
+     document.getElementById('cancel')
+          .addEventListener('click', () =>
+          {
+               if (document.getElementById('cancel')
+                    .innerHTML == 'Cancel')
+               {
+                    window.close();
+               }
+               else // user wants to delete to a bookmark
+               {
+                    storage.get()
+                         .then(data =>
+                         {
+                              rename_button_names();
+                              const indexBookmark = data.bookmarks.findIndex(
+                                   item => item['url'] === currentUrl
+                              );
+                              
+                              data.bookmarks.splice(indexBookmark, 1);
+                              storage.set(
+                              {
+                                   bookmarks: data.bookmarks
+                              });
+                              document.getElementById('description')
+                                   .value = "";
+                         });
+               }
+          });
+     
+     document.getElementById('add')
+          .addEventListener('click', () =>
+          {
+               insert_to_storage(currentUrl);
+          });
+     
+     commands.onCommand.addListener(command =>
+     {
+          if (command == 'enter_action')
+          {
+               insert_to_storage(currentUrl);
           }
      });
-
-     document.getElementById('add').addEventListener('click', () => {
-          insert_to_storage( currentUrl );
-     });
-
-     commands.onCommand.addListener( command => {
-          if ( command == 'enter_action') {
-               insert_to_storage( currentUrl );
-          }
-     } );
 }
 
 tabs.query(
      {
-          currentWindow: true,
-          active: true
+          currentWindow: true
+          , active: true
      })
-     .then((tabs) =>
+     .then( /*to get URL and a title of an active tab*/ (tabs) =>
      {
           
           let status = document.getElementById('status');
           status.innerHTML = 'Add bookmark';
           
-          document.getElementById('title').defaultValue = tabs[0].title;
-          setup_click_action( tabs[0].url );
+          document.getElementById('title')
+               .defaultValue = tabs[0].title;
 
-          storage.get().then(data =>
+          setup_click_action(tabs[0].url);
+          
+          storage.get()
+               .then(data =>
                {
-               if ( !data.bookmarks )
-               {
-                    return;
-               }
-
-               const indexBookmark = data.bookmarks.findIndex(
-                    item => item['url'] === tabs[0].url);
-
-               let buttonSubmit = document.getElementById('add');
-               let buttonReset = document.getElementById('cancel');
-               if (indexBookmark == -1)
-               {
-                    buttonSubmit.firstChild.data = 'Add';
-                    buttonReset.firstChild.data = 'Cancel'
-               }
-               else
-               {
-                    document.getElementById('title').value = data.bookmarks[indexBookmark].title;
-                    document.getElementById('description').value = data.bookmarks[indexBookmark].description;
-                    status.innerHTML = 'Edit bookmark';
-                    buttonSubmit.firstChild.data = 'Edit';
-                    buttonReset.firstChild.data = 'Remove';
-             }
-          })
-     }
-);
+                    if (!data.bookmarks)
+                    {
+                         // storage is empty and
+                         // buttons already configured
+                         return;
+                    }
+                    
+                    const indexBookmark = data.bookmarks.findIndex(
+                         item => item['url'] === tabs[0].url);
+                    
+                    // configure buttons
+                    
+                    let buttonSubmit = document.getElementById('add');
+                    let buttonReset = document.getElementById('cancel');
+                    if (indexBookmark == -1)
+                    {
+                         buttonSubmit.firstChild.data = 'Add';
+                         buttonReset.firstChild.data = 'Cancel'
+                    }
+                    else
+                    {
+                         document.getElementById('title')
+                              .value = data.bookmarks[indexBookmark].title;
+                         document.getElementById('description')
+                              .value = data.bookmarks[indexBookmark].description;
+                         status.innerHTML = 'Edit bookmark';
+                         buttonSubmit.firstChild.data = 'Edit';
+                         buttonReset.firstChild.data = 'Remove';
+                    }
+               })
+     });
--- storage/storage.js
+++ storage/storage.js
@@ -7,20 +7,13 @@ const Div_table_view = 'table_view';
 const Table_view_select_id = 'sort';
 
 const Div_table = 'container';
-const Table_id = 'uniq';
+const Table_id = 'table_id';
 
-
-/*
-@todo: format all files + add comment
-     + changes in css
-*/
- 
-
-// generate a link to export storage
+/// @brief creates a link to export data from the storage in JSON format
 function generate_download_link(bookmarks)
 {
-     var blob = new Blob([JSON.stringify(bookmarks)],
-     {
+     var blob = new Blob([JSON.stringify(bookmarks)]
+     , {
           type: 'application/json'
      });
      var url = URL.createObjectURL(blob);
@@ -28,44 +21,50 @@ function generate_download_link(bookmarks)
      a.href = url;
      a.download = Download_file_name;
      a.textContent = 'Download ' + Download_file_name;
-     document.getElementById(Div_download).appendChild(a);
+     document.getElementById(Div_download)
+          .appendChild(a);
 }
 
-// generate a table view
+/// @brief creates a selection for displaying tables
+/// @param bookmarks - data that is stored in the storage
 function generate_select(bookmarks)
 {
      var label = document.createElement('label');
      label.setAttribute('for', Div_table_view);
      label.innerHTML = 'Table sorted by: ';
      var previous;
-
+     
      var select = document.createElement('select');
      select.setAttribute('id', Table_view_select_id);
      select.add(new Option('Time of adding', 'added'));
      select.add(new Option('URL', 'url'));
      select.add(new Option('Title', 'title'));
-
+     
      let common = document.getElementById(Div_table_view);
      common.appendChild(label);
      common.appendChild(select);
 }
 
-function popup_setup_accept(bookmarks)
+/// @brief connects a selection click to a table display
+/// @param bookmarks - data that is stored in the storage
+function setup_sort_action(bookmarks)
 {
-     document.getElementById(Div_table_view).addEventListener('change', function()
-     {
-          if (!bookmarks)
+     document.getElementById(Div_table_view)
+          .addEventListener('change', function ()
           {
-               console.warn('storage is empty');
-               return;
-          }
-          let sort_type = document.getElementById(Table_view_select_id).value;
-          switch (sort_type)
-          {
+               if (!bookmarks)
+               {
+                    console.warn('storage is empty');
+                    return;
+               }
+               let sort_type = document.getElementById(Table_view_select_id)
+                    .value;
+               switch (sort_type)
+               {
                case 'url':
                case 'title':
                {
-                    bookmarks.sort(function(lhs, rhs)
+                    bookmarks.sort(function (lhs, rhs)
                     {
                          return lhs[sort_type].localeCompare(rhs[sort_type]);
                     });
@@ -73,34 +72,39 @@ function popup_setup_accept(bookmarks)
                }
                case 'added':
                {
-                    bookmarks.sort(function(lhs, rhs)
+                    bookmarks.sort(function (lhs, rhs)
                     {
-
+                         
                          return lhs['date'][sort_type].localeCompare(rhs['date'][sort_type]);
                     });
-
+                    
                     break;
                }
                default:
                     console.log('not implemented');
-          }
-
-          document.getElementById(Table_id).remove();
-          generate_table(bookmarks);
-     });
+               }
+               
+               document.getElementById(Table_id)
+                    .remove();
+               generate_table(bookmarks);
+          });
 }
 
+/// @brief creates HTML-table by data
+/// @param bookmarks - data that is stored in the storage
 function generate_table(bookmarks)
 {
      let container = document.getElementById(Div_table);
      let table = document.createElement('table');
      table.setAttribute('id', Table_id);
+     // creating a table description 
      let caption = document.createElement('caption');
      caption.innerHTML = 'Bookmark storage';
-
+     
      container.appendChild(table);
      table.appendChild(caption);
-
+     
+     // creating headers of each columns
      let thead = table.createTHead();
      let row = thead.insertRow();
      for (let str of Object.keys(bookmarks[0]))
@@ -110,11 +114,12 @@ function generate_table(bookmarks)
           th.appendChild(text);
           row.appendChild(th);
      }
-
+     
+     // creating each row based on the data in the storage
      for (let [index, element] of bookmarks.entries())
      {
           let row = table.insertRow();
-          row.setAttribute('id', index);
+          row.setAttribute('id', index); // for removal a row
           for (key in element)
           {
                let cell = row.insertCell();
@@ -122,29 +127,33 @@ function generate_table(bookmarks)
                let text = document.createTextNode(element[key]);
                switch (key)
                {
-                    case 'url':
-                         let a = document.createElement('a');
-                         a.appendChild(text);
-                         a.href = element[key];
-                         cell.appendChild(a);
-                         break;
-                    case 'date':
-                         cell.appendChild(document.createTextNode('Added time: ' + element[key]['added']));
-                         if (element[key]['changed'].length !== 0)
-                         {
-                              cell.appendChild(document.createElement('br'));
-                              cell.appendChild(document.createTextNode('Last edit time: ' + element[key]['changed']));
-                         }
-                         break;
-                    default:
-                         cell.setAttribute('class', 'editable');
-                         cell.appendChild(text);
+               case 'url':
+                    // it should be a link
+                    let a = document.createElement('a');
+                    a.appendChild(text);
+                    a.href = element[key];
+                    cell.appendChild(a);
+                    break;
+               case 'date':
+                    cell.appendChild(document.createTextNode('Added time: ' + element[key]['added']));
+                    if (element[key]['changed'].length !== 0)
+                    {
+                         cell.appendChild(document.createElement('br'));
+                         cell.appendChild(document.createTextNode('Last edit time: ' + element[key]['changed']));
+                    }
+                    break;
+               default:
+                    // title, description
+                    cell.setAttribute('class', 'editable');
+                    cell.appendChild(text);
                }
           }
+
+          // event for edit button
           let buttonEdit = document.createElement('button');
           buttonEdit.type = 'button';
           buttonEdit.innerHTML = 'Edit';
-          buttonEdit.onclick = function()
+          buttonEdit.onclick = function ()
           {
                if (buttonEdit.innerHTML === 'Edit')
                {
@@ -156,19 +165,24 @@ function generate_table(bookmarks)
                               text.setAttribute('contenteditable', true);
                          }
                     }
-                    row.cells.namedItem('description').focus();
+                    // set default focus to description
+                    row.cells.namedItem('description')
+                         .focus();
                }
                else
                {
                     buttonEdit.innerHTML = 'Edit';
+                    // the row should not change now
                     row.setAttribute('contenteditable', false);
+
                     storage.get()
                          .then(data =>
                          {
                               let indexToEdit = bookmarks.findIndex(
-                                             item => item['url'] === row.cells.namedItem('url').textContent
+                                   item => item['url'] === row.cells.namedItem('url')
+                                   .textContent
                               );
-
+                              
                               if (indexToEdit == -1)
                               {
                                    console.error('Bookmark not found');
@@ -181,43 +195,47 @@ function generate_table(bookmarks)
                                    if (text.className == 'editable')
                                    {
                                         let content = text.textContent;
-                                        if ( bookmarks[indexToEdit][text.getAttribute('id')] != content )
+                                        if (bookmarks[indexToEdit][text.getAttribute('id')] != content)
                                         {
                                              changed = true;
                                              bookmarks[indexToEdit][text.getAttribute('id')] = text.textContent;
                                         }
                                    }
                               }
-                              if ( changed )
+                              if (changed)
                               {
-                                   let editTime = new Date().toLocaleString();
-
-                                   let currentCell = row.cells.namedItem( 'date' );
-                                   if ( bookmarks[indexToEdit]['date']['changed'].length != 0 ) {
-                                        currentCell.lastChild.textContent = ( 'Last edit time: ' + editTime );
-                                   } else { 
-                                        currentCell.appendChild( document.createElement( 'br' ) );
+                                   let editTime = new Date()
+                                        .toLocaleString();
+                                   
+                                   let currentCell = row.cells.namedItem('date');
+                                   if (bookmarks[indexToEdit]['date']['changed'].length != 0)
+                                   {
+                                        currentCell.lastChild.textContent = ('Last edit time: ' + editTime);
+                                   }
+                                   else
+                                   {
+                                        currentCell.appendChild(document.createElement('br'));
                                         currentCell.appendChild(
-                                             document.createTextNode( 'Last edit time: ' + editTime )
+                                             document.createTextNode('Last edit time: ' + editTime)
                                         );
                                    }
-
+                                   
                                    bookmarks[indexToEdit]['date']['changed'] = editTime;
-
+                                   
                                    storage.set(
-                                        {
-                                             bookmarks: bookmarks
-                                        });
+                                   {
+                                        bookmarks: bookmarks
+                                   });
                               }
                          });
                }
           };
-
-          // button for delete 
+          
+          // event for delete button
           let button = document.createElement('button');
           button.type = 'button';
           button.innerHTML = 'Delete';
-          button.onclick = function()
+          button.onclick = function ()
           {
                storage.get()
                     .then(data =>
@@ -225,7 +243,7 @@ function generate_table(bookmarks)
                          const indexToDelete = bookmarks.findIndex(
                               item => item['url'] === element['url']
                          );
-
+                         
                          if (indexToDelete != -1)
                          {
                               bookmarks.splice(indexToDelete, 1);
@@ -233,8 +251,6 @@ function generate_table(bookmarks)
                               {
                                    bookmarks: bookmarks
                               });
-                              document.getElementById(index)
-                                   .remove();
                               if (bookmarks.length === 0)
                               {
                                    window.location.reload();
@@ -248,11 +264,22 @@ function generate_table(bookmarks)
      }
 }
 
-function popup_setup_error()
+/// @brief generates the page if the storage is empty
+function page_setup_ERROR()
 {
      let header = document.createElement('h1');
      header.innerHTML = 'Storage is empty';
      document.body.append(header);
+}
+
+/// @brief generates the page if the storage is not empty
+/// @param bookmarks 
+function page_setup_OK( bookmarks )
+{
+     generate_select( bookmarks );
+     generate_download_link( bookmarks );
+     generate_table( bookmarks );
+     setup_sort_action( bookmarks );
 }
 
 storage.get()
@@ -260,12 +287,8 @@ storage.get()
      {
           if (!data.bookmarks || data.bookmarks.length === 0)
           {
-               popup_setup_error();
+               page_setup_ERROR();
                return;
           }
-          generate_select(data.bookmarks);
-          generate_download_link(data.bookmarks);
-          generate_table(data.bookmarks);
-          popup_setup_accept(data.bookmarks);
+          page_setup_OK( data.bookmarks );
      });
-
